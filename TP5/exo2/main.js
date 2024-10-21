421714c6271c739bdfe1af07c8202883e3bd0a40
Ayoubned <ayoubkhalied@gmail.com>
2024-10-21 12:02:29 +0200

    suite exo 2

--- TP5/exo2/main.js
+++ TP5/exo2/main.js
@@ -19,4 +19,60 @@ $(document).ready(function () {
             }
         ]
     });
+
+    $('#userForm').on('submit', function (e) {
+        e.preventDefault();
+        var name = $('#name').val();
+        var email = $('#email').val();
+
+        $.ajax({
+            url: 'http://localhost/IDAW/TP4/REST_users.php',
+            type: 'POST',
+            contentType: 'application/json',
+            data: JSON.stringify({ name: name, email: email }),
+            success: function (response) {
+                alert('Utilisateur ajouté avec succès');
+                table.ajax.reload();
+            },
+            error: function () {
+                alert('Erreur lors de l\'ajout de l\'utilisateur');
+            }
+        });
+    });
+
+    $('#usersTable').on('click', '.delete-btn', function () {
+        var userId = $(this).data('id');
+
+        $.ajax({
+            url: `http://localhost/IDAW/TP4/REST_users.php?id=${userId}`,
+            type: 'DELETE',
+            success: function (response) {
+                alert('Utilisateur supprimé');
+                table.ajax.reload();
+            },
+            error: function () {
+                alert('Erreur lors de la suppression');
+            }
+        });
+    });
+
+    $('#usersTable').on('click', '.edit-btn', function () {
+        var userId = $(this).data('id');
+        var newName = prompt('Entrez le nouveau nom');
+        var newEmail = prompt('Entrez le nouvel email');
+
+        $.ajax({
+            url: `http://localhost/IDAW/TP4/REST_users.php?id=${userId}`,
+            type: 'PUT',
+            contentType: 'application/json',
+            data: JSON.stringify({ name: newName, email: newEmail }),
+            success: function (response) {
+                alert('Utilisateur mis à jour');
+                table.ajax.reload();
+            },
+            error: function () {
+                alert('Erreur lors de la mise à jour');
+            }
+        });
+    });
 });
